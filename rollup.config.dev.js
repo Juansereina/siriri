1b0df4fe23907f0c8d1408f62a177152150c1ab5
Juan José Rendón <juanjoserendon13@outlook.com>
2020-03-29 21:34:46 -0500

    add compiler to simulate prod env

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,6 +1,10 @@
 // rollup.config.js
 const json = require('@rollup/plugin-json');
+const { terser } = require('rollup-plugin-terser');
 const del = require('rollup-plugin-delete');
+const babel = require('rollup-plugin-babel');
+const resolve = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
 
 module.exports = {
   input: 'src/js/index.js',
@@ -8,11 +12,15 @@ module.exports = {
     {
       dir: 'bundlers/bundle',
       format: 'esm',
-      sourcemap: true
+      sourcemap: true,
+      plugins: [terser()]
     }
   ],
   plugins: [
     json(),
+    resolve(),
+    commonjs(),
+    babel({ runtimeHelpers: true }),
     del({ targets: 'bundlers/bundle/*' })
   ],
   watch: {
