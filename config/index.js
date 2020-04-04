21c57c285400b292166b10577892b9bfd89b1a70
Juansereina <juansereina@outlook.com>
2020-04-04 14:31:31 -0500

    Handle prod config
    Signed-off-by: Juansereina <juansereina@outlook.com>

--- config/index.js
+++ config/index.js
@@ -1,9 +1,14 @@
 /*eslint-env node*/
 const yaml = require('js-yaml');
 const fs   = require('fs');
+const merge = require('deepmerge')
 
+/* Helpers */
 const production = process.env.NODE_ENV === 'production';
-const source = production ? 'production' : 'default';
-const config = yaml.safeLoad(fs.readFileSync(`./config/${source}.yml`, 'utf8'));
+const readFile = (file, read = true) => read ? yaml.safeLoad(fs.readFileSync(`./config/${file}.yml`, 'utf8')) : {};
 
-module.exports = config;
+/* config files */
+const basic = readFile('default')
+const prod =  readFile('production', production);
+
+module.exports = merge(basic, prod);
