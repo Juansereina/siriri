f3e7b37f63a3edc4541602ae20b7daa4ccdd3a0f
Juansereina <juansereina@outlook.com>
2020-04-05 11:54:36 -0500

    Adding state color
    Signed-off-by: Juansereina <juansereina@outlook.com>

--- src/js/components/room.js
+++ src/js/components/room.js
@@ -2,6 +2,7 @@ import { getData } from '../services/storage';
 
 const createUser = ({name, state}) => `
 <li class="user">
+  <div class="user__color-state"></div>
   <span class="user__name">name: <b>${name}</b></span>
   <span class="user__state">state: <b>${state}</b></span>
 </li>
